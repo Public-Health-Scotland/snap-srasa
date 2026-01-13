d2f4b41a93c2acae1dcfcf4866096ee8ba18517f
Dylan <dylan.lewis@phs.scot>
2026-01-13 17:26:50 +0000

    move around control script

--- 04_reports/management_report/serverless_html/mgmt_report_control.R
+++ 04_reports/management_report/serverless_html/mgmt_report_control.R
@@ -15,17 +15,6 @@ mgmt_data_dir <- paste0(data_dir, "management_report/")
 script_dir <- "./04_reports/management_report/serverless_html/"
 output_dir <- "/conf/quality/srasa/(04) Project Reports/Monthly Reports/Management Report/"
 
-# data --------------------------------------------------------------------
-
-util_procsmth <- read_parquet(paste0(mgmt_data_dir, "util_procsmth.parquet"))
-util_procsday <- read_parquet(paste0(mgmt_data_dir, "util_procsday.parquet"))
-
-equity_agesex <- read_parquet(paste0(mgmt_data_dir, "equity_agesex.parquet"))
-equity_agemean <- read_parquet(paste0(mgmt_data_dir, "equity_agemean.parquet"))
-equity_simd <- read_parquet(paste0(mgmt_data_dir, "equity_simd.parquet"))
-equity_resprop <- read_parquet(paste0(mgmt_data_dir, "equity_resprop.parquet"))
-
-spec_procsmth <- read_parquet(paste0(mgmt_data_dir, "spec_procsmth.parquet"))
 
 
 # functions ---------------------------------------------------------------
@@ -33,10 +22,7 @@ spec_procsmth <- read_parquet(paste0(mgmt_data_dir, "spec_procsmth.parquet"))
 source(paste0(script_dir, "mgmt_report_html_funcs.R"))
 source(paste0(script_dir, "mgmt_report_plots.R"))
 
-# report ------------------------------------------------------------------
-
-#test_report <- produce_report("Greater Glasgow & Clyde", "2024-10-01", "2025-10-01")
-#save_self_contained_html(test_report, paste0(output_dir, "test_new_func.html"))
+# controls ----------------------------------------------------------------
 
 date_to <- as.Date("2025-10-01")
 date_from <- date_to %m-% months(12)
@@ -50,6 +36,27 @@ health_boards <- list(
   'Lothian',
   'Tayside'
 )
+
+# data --------------------------------------------------------------------
+
+util_procsmth <- read_parquet(paste0(mgmt_data_dir, "util_procsmth.parquet")) |>
+  filter(op_mth < date_to, op_mth >= date_from)
+util_procsday <- read_parquet(paste0(mgmt_data_dir, "util_procsday.parquet")) |>
+  filter(op_mth < date_to, op_mth >= date_from)
+
+equity_agesex <- read_parquet(paste0(mgmt_data_dir, "equity_agesex.parquet"))
+equity_agemean <- read_parquet(paste0(mgmt_data_dir, "equity_agemean.parquet"))
+equity_simd <- read_parquet(paste0(mgmt_data_dir, "equity_simd.parquet"))
+equity_resprop <- read_parquet(paste0(mgmt_data_dir, "equity_resprop.parquet"))
+
+spec_procsmth <- read_parquet(paste0(mgmt_data_dir, "spec_procsmth.parquet")) |>
+  filter(op_mth_year < date_to, op_mth_year >= date_from)
+
+
+# reports -----------------------------------------------------------------
+
+#test_report <- produce_report("Greater Glasgow & Clyde", "2024-10-01", "2025-10-01")
+#save_self_contained_html(test_report, paste0(output_dir, "test_new_func.html"))
 
 batch_reports(
   health_boards,
