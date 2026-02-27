c2a81534f2aa681c979b6ac69ef463d05507f822
Dylan <dylan.lewis@phs.scot>
2026-02-27 15:37:05 +0000

    complete() phaseprocs data to ensure plots render for all hosps / specialties

--- 04_reports/management_report/serverless_html/mgmt_report_plots.R
+++ 04_reports/management_report/serverless_html/mgmt_report_plots.R
@@ -96,29 +96,44 @@ make_plot_spec_procsmth <- function(hospitals, spec_colours){
 make_plot_spec_procphase <- function(hospitals, specialty){ #this one needs specialty tabs
   #"Number of procedures performed by RAS per month by procedure phase, by specialty ({start_date} - {latest_date})", (already in there)
   
+  all_months <- spec_procphase$op_mth |> unique() |> sort()
+  
   chart_data <- spec_procphase %>% 
-    filter(ras_proc == "RAS" &
-             hospital_name_grp %in% hospitals) %>% 
+    filter(ras_proc == "RAS",
+           hospital_name_grp %in% hospitals,
+           main_op_specialty == specialty) %>%
+    complete(op_mth = all_months,
+             main_op_phase = c("phase1", "phase2", "non-priority"),
+             hospital_name_grp = hospitals,
+             fill = list(n = 0)) %>% 
     mutate(main_op_phase = factor(main_op_phase, 
-                                  levels = c("phase1", "phase2", "non-priority")),
-           op_mth = format(op_mth, "%Y-%m")) %>% 
-    filter(main_op_specialty == specialty)
+                                  levels = c("phase1", "phase2", "non-priority"),
+                                  labels = c("Phase 1", "Phase 2", "Non-priority")),
+           op_mth = as_date(op_mth))
   
   spec_procphase_plot <- ggplot(chart_data, 
                                 aes(x = op_mth, y = n, fill = fct_rev(main_op_phase),
                                     tooltip = paste0("Hospital Location: ", hospital_name_grp,
-                                                     "\n Procedure phase; ", main_op_phase,
+                                                     "\n Procedure phase: ", main_op_phase,
                                                      "\n No. RAS procedures: ", n,
                                                      "\n Month: ", op_mth),
                                     data_id = op_mth)) +
-    geom_bar_interactive(stat = "identity", hover_nearest = TRUE) +
+    geom_bar_interactive(stat = "identity", width = 20, hover_nearest = TRUE) +
     labs(x = "Month", 
          y = "Total RAS procedures", 
          fill = "Procedure phase",
          caption = "Data from SMR01",
          subtitle = paste0())+ 
-    scale_fill_manual(values = c("#b1b1b1","#3F085C","#3E8ECC"))+
-    facet_wrap(~hospital_name_grp)+
+    scale_fill_manual(values = c("Non-priority" = "#b1b1b1","Phase 1" = "#3F085C", "Phase 2" = "#3E8ECC")) + 
+    scale_y_continuous(
+      breaks = scales::breaks_width(5),
+    ) +
+    scale_x_date(
+      date_breaks = "1 month",
+      date_labels = "%b %Y"
+    ) +
+    expand_limits(y = 5) +
+    facet_wrap(~hospital_name_grp) +
     theme_phs_ylines() +
     theme(legend.position = 'bottom',
           axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
