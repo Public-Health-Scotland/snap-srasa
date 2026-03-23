d5cd801646c6d280adbeb994efca0bdababe92d4
Bex Madden <bex.madden@phs.scot>
2026-03-23 12:35:34 +0000

    follow-up minor queries to data query for highland colorectal

--- 06_data_requests/2026-03-12_highland_data_query.R
+++ 06_data_requests/2026-03-12_highland_data_query.R
@@ -19,3 +19,18 @@ highland_data <- read_parquet(paste0(data_dir, "monthly_extract/srasa_smr_extrac
 write.csv(highland_data, "../../../(08) Communications/Data Query/2026-03-12_highland_data_query_srasa.csv")
 
 # Send out data query with 'data_dictionary_for_data_query', 'diag_codes_other_spec'/gynae/ent, and 'RAS procedure codes and phasing' docs
+
+# havign made fix to specialty for unlisted procs, checking same dates to see if gynae procedures have correctly made it out of 'general'
+highland_data <- read_parquet(paste0(data_dir, "monthly_extract/srasa_smr_extract_min.parquet")) %>% #extract made 19/2/26 with new specialty coding
+  filter(hosp_health_board == "Highland" &
+           (op_mth == "2025-03-01") &
+           (main_op_specialty == "Colorectal" | main_op_specialty == "General surgery" | main_op_specialty == "General surgery - unlisted" | main_op_specialty == "Gynaecology"))
+#gynae procedures have succesfully been recoded to the right specialty
+
+t <- highland_data %>% 
+  filter(upi_number %in% c("1705595421", "0103483578", "2601469021")) #only finds 1st 2 - i RAS conv open 1 NOS (MIA conv open in op2a position doesn't make it to main_op_approach)
+ # filter(main_op_approach == "RAS conv open" | main_op_approach == "MIA conv open")
+ # filter(op_mth == "2025-12-01" & main_op_specialty == "Colorectal")
+
+big_data <- read_parquet("../testing/part_processed_extract_18-3-26.parquet") %>% 
+  filter(upi_number == "2601469021") #not present in data at all... wrong chi or record not uploaded yet? surgery from december
